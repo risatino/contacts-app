a54f425e1c05c851a2b3fa97ab780d40bd1ee80f
risatino <charissatolentino@gmail.com>
2016-09-01 12:35:41 -0500

    model and added three contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,9 @@
 class ContactsController < ApplicationController
+  def all_contacts
+    # @contacts = Contact.all
+  end
+
+  def one_contact
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ get '/all' => 'contacts#all_contacts'
+ get '/contact_data' => 'contacts#one_contact'
 end
